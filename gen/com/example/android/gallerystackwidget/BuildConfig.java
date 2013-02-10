17a2cd2f85d0e8bb2eff47727b63d345d02afac7
seungwoonlee <bluecrck@gmail.com>
2013-02-11 00:58:34 +0900

    Copy from Android StackWidget Sample

---
+++ gen/com/example/android/gallerystackwidget/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.android.gallerystackwidget;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
